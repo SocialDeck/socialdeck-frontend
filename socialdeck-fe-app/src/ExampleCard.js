a2e2fdb5dfc7f3fe3b94d8a3556b9520f18ce92c
tcwhitak <t.chad.whitaker@gmail.com>
2018-10-07 12:30:43 -0400

    added link to return to contacts after viewing example card

--- socialdeck-fe-app/src/ExampleCard.js
+++ socialdeck-fe-app/src/ExampleCard.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react'
+import { Link } from '@reach/router'
 
 class ExampleCard extends Component {
   render () {
     return (
-      <div className='card'>
-        <div className='cardLine'><i className='fas fa-user-circle cardIcon' /> Name</div>
-        <div className='cardLine'><i className='fas fa-briefcase cardIcon' /> Business</div>
-        <div className='cardLine'><i className='fas fa-phone cardIcon' /> 999-999-9999</div>
-        <div className='cardLine'><i className='fas fa-envelope cardIcon' /> example@example.com</div>
-        <div className='address'>
-          <div className='addressIconWrapper'><i className='fas fa-map-marked cardIcon' /></div>
-          <div className='addressBlock'>
-            <div className='addressLine line1'> Address Line 1</div>
-            <div className='addressLine line2'> Address Line 2</div>
-            <div className='addressLine line3'> City, State ZipCode</div>
+      <React.Fragment>
+        <Link to='/contacts'>Contacts List</Link>
+        <div className='card'>
+          <div className='cardLine'><i className='fas fa-user-circle cardIcon' /> Name</div>
+          <div className='cardLine'><i className='fas fa-briefcase cardIcon' /> Business</div>
+          <div className='cardLine'><i className='fas fa-phone cardIcon' /> 999-999-9999</div>
+          <div className='cardLine'><i className='fas fa-envelope cardIcon' /> example@example.com</div>
+          <div className='address'>
+            <div className='addressIconWrapper'><i className='fas fa-map-marked cardIcon' /></div>
+            <div className='addressBlock'>
+              <div className='addressLine line1'> Address Line 1</div>
+              <div className='addressLine line2'> Address Line 2</div>
+              <div className='addressLine line3'> City, State ZipCode</div>
+            </div>
           </div>
         </div>
-      </div>
+      </React.Fragment>
+
     )
   }
 }
