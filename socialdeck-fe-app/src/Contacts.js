b3bbfac3a7a30f9ee3f83fa6cfbd4de291da7f88
tcwhitak <t.chad.whitaker@gmail.com>
2018-10-10 15:54:51 -0400

    fixed list background color

--- socialdeck-fe-app/src/Contacts.js
+++ socialdeck-fe-app/src/Contacts.js
@@ -4,68 +4,65 @@ import { Link, Redirect } from '@reach/router'
 import { GET_CONTACTS } from './queries'
 
 class Contacts extends Component {
-  stripNumber(number) {
+  stripNumber (number) {
     if (number) {
-      return number.replace(/\D+/g, "").replace(/^[01]/, "")
+      return number.replace(/\D+/g, '').replace(/^[01]/, '')
     } else {
-      return
+
     }
   }
-  parseAddress(address) {
+  parseAddress (address) {
     if (address) {
-      return address.address1.replace(" ", "+") + ",+" + address.city + ",+" + address.state + "+" + address.postalCode
+      return address.address1.replace(' ', '+') + ',+' + address.city + ',+' + address.state + '+' + address.postalCode
     } else {
-      return
+
     }
   }
-  render() {
+  render () {
     const token = window.localStorage.getItem('token')
     return <React.Fragment>{token
-      ? <div className='contactsLinks'>
-        <h2>Contacts</h2>
-        <ul className="list" >
-          <Query
-            query={GET_CONTACTS} variables={{ token: token }}
-          >
-            {({ loading, error, data }) => {
-              if (loading) return <p>Loading...</p>
-              if (error) return <p>Error :(</p>
+      ? <ul className='list' >
+        <Query
+          query={GET_CONTACTS} variables={{ token: token }}
+        >
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>
+            if (error) return <p>Error :(</p>
 
-              return data.contacts.map((contact, idx) => (
+            return data.contacts.map((contact, idx) => (
 
-                < li key={idx} className="list-item" >
-                  <Link to={contact.cardToken} key={idx} className='list-item contact'>
-                    < div className="list-item__left" >
-                      < div className="list-item__thumbnail monogram" >
-                        {contact.name[0]}
-                      </div>
+              <li key={idx} className='list-item' >
+                <Link to={contact.cardToken} key={idx} className='list-item contact'>
+                  <div className='list-item__left' >
+                    <div className='list-item__thumbnail monogram' >
+                      {contact.name[0]}
                     </div>
-
-                    <div className='list-item__center'>
-                      < div className="list-item__title" > {contact.name} </div>
-                      < div className="list-item__subtitle" > {contact.displayName} </div>
-                    </div>
-                  </Link>
-                  < div className="list-item__right" >
-                    {contact.number && <a href={"tel:+" + this.stripNumber(contact.number)}> <i className='fas fa-phone list-item__icon'> </i></a>}
-                    {!contact.number && <i className='fas fa-phone list-item__hidden'> </i>}
-                    {contact.mobile && <a href={"sms:+" + this.stripNumber(contact.number)}> <i className='fas fa-comment-alt list-item__icon'> </i></a>}
-                    {!contact.mobile && <i className='fas fa-comment-alt list-item__hidden'> </i>}
-                    {contact.address && <a href={"https://maps.google.com/maps/place/" + this.parseAddress(contact.address)}><i className='fas fa-directions list-item__icon'> </i></a>}
-                    {!contact.address && <i className='fas fa-directions list-item__hidden'> </i>}
-                    {contact.email && <a href={"mailto:" + contact.email}><i className='fas fa-envelope list-item__icon'> </i></a>}
-                    {!contact.email && <i className='fas fa-envelope list-item__hidden'> </i>}
                   </div>
-                </li>
 
-              ))
+                  <div className='list-item__center'>
+                    <div className='list-item__title' > {contact.name} </div>
+                    <div className='list-item__subtitle' > {contact.displayName} </div>
+                  </div>
+                </Link>
+                <div className='list-item__right' >
+                  {contact.number && <a href={'tel:+' + this.stripNumber(contact.number)}> <i className='fas fa-phone list-item__icon' /></a>}
+                  {!contact.number && <i className='fas fa-phone list-item__hidden' />}
+                  {contact.mobile && <a href={'sms:+' + this.stripNumber(contact.number)}> <i className='fas fa-comment-alt list-item__icon' /></a>}
+                  {!contact.mobile && <i className='fas fa-comment-alt list-item__hidden' />}
+                  {contact.address && <a href={'https://maps.google.com/maps/place/' + this.parseAddress(contact.address)}><i className='fas fa-directions list-item__icon' /></a>}
+                  {!contact.address && <i className='fas fa-directions list-item__hidden' />}
+                  {contact.email && <a href={'mailto:' + contact.email}><i className='fas fa-envelope list-item__icon' /></a>}
+                  {!contact.email && <i className='fas fa-envelope list-item__hidden' />}
+                </div>
+              </li>
 
-            }
+            ))
+          }
 
-            }
-          </Query>
-        </ul>
-      </div>
+          }
+        </Query>
+      </ul>
+
       : <Redirect to='/login' noThrow />
     }
     </React.Fragment>
