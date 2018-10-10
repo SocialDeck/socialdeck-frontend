d53847a0fbfbcffecbef79c74dd5c57e4f7aad0f
tcwhitak <t.chad.whitaker@gmail.com>
2018-10-10 11:34:36 -0400

    committed to shut down laptop

--- socialdeck-fe-app/src/Home.js
+++ socialdeck-fe-app/src/Home.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
-import { Redirect } from '@reach/router'
+import { Link } from '@reach/router'
 
 class Home extends Component {
   render () {
     const token = window.localStorage.getItem('token')
     return (
       <div className='home'>
-        { !token && <Redirect to='/login' noThrow />}
         <nav className='navigation'>
-          <h1>Social Deck</h1>
-          {token && <button onClick={() => this.props.logOut()}>Log Out</button>}
+          <div className='topRow'>
+            <h1>Social Deck</h1>
+          </div>
+
+          { !token && <Link to='/login'>Login</Link>}
+          {token && <React.Fragment>
+            <Link to='/contacts/connections'>Connections</Link>
+            <Link to='/my-cards'>My Cards</Link>
+            <Link to='/contacts/new-contact'>New Contact</Link>
+            <a className='logOutButton'onClick={() => this.props.logOut()}>Log Out</a>
+          </React.Fragment>}
         </nav>
 
         {this.props.children}
