53ae994dab68aae4d67f7d8bde5a580d43d92b88
tcwhitak <t.chad.whitaker@gmail.com>
2018-10-12 11:54:10 -0400

    fix android google map app bug

--- socialdeck-fe-app/src/Components/Contacts.js
+++ socialdeck-fe-app/src/Components/Contacts.js
@@ -49,7 +49,7 @@ class Contacts extends Component {
                   {!contact.number && <i className='fas fa-phone list-item__hidden' />}
                   {contact.mobile && <a href={'sms:' + this.stripNumber(contact.number)}> <i className='fas fa-comment-alt list-item__icon' /></a>}
                   {!contact.mobile && <i className='fas fa-comment-alt list-item__hidden' />}
-                  {contact.address && <a href={'https://maps.google.com/maps/place/' + this.parseAddress(contact.address)}><i className='fas fa-directions list-item__icon' /></a>}
+                  {contact.address && <a href={'http://maps.apple.com/?address=' + this.parseAddress(contact.address)}><i className='fas fa-directions list-item__icon' /></a>}
                   {!contact.address && <i className='fas fa-directions list-item__hidden' />}
                   {contact.email && <a href={'mailto:' + contact.email}><i className='fas fa-envelope list-item__icon' /></a>}
                   {!contact.email && <i className='fas fa-envelope list-item__hidden' />}
