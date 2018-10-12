f86eebad5907c7af07c117080891d4a0a949b432
tcwhitak <t.chad.whitaker@gmail.com>
2018-10-12 13:53:05 -0400

    fix icon colors on contacts and cards

--- socialdeck-fe-app/src/Components/Favorites.js
+++ socialdeck-fe-app/src/Components/Favorites.js
@@ -45,13 +45,13 @@ class Favorites extends Component {
                   </div>
                 </Link>
                 <div className='list-item__right' >
-                  {contact.number && <a href={'tel:' + this.stripNumber(contact.number)}> <i className='fas fa-phone list-item__icon' /></a>}
+                  {contact.number && <a href={'tel:' + this.stripNumber(contact.number)}> <i className='fas fa-phone list-item__icon standardIcon' /></a>}
                   {!contact.number && <i className='fas fa-phone list-item__hidden' />}
-                  {contact.mobile && <a href={'sms:' + this.stripNumber(contact.number)}> <i className='fas fa-comment-alt list-item__icon' /></a>}
+                  {contact.mobile && <a href={'sms:' + this.stripNumber(contact.number)}> <i className='fas fa-comment-alt list-item__icon standardIcon' /></a>}
                   {!contact.mobile && <i className='fas fa-comment-alt list-item__hidden' />}
-                  {contact.address && <a href={'http://maps.apple.com/?address=' + this.parseAddress(contact.address)}><i className='fas fa-directions list-item__icon' /></a>}
+                  {contact.address && <a href={'http://maps.apple.com/?address=' + this.parseAddress(contact.address)}><i className='fas fa-directions list-item__icon standardIcon' /></a>}
                   {!contact.address && <i className='fas fa-directions list-item__hidden' />}
-                  {contact.email && <a href={'mailto:' + contact.email}><i className='fas fa-envelope list-item__icon' /></a>}
+                  {contact.email && <a href={'mailto:' + contact.email}><i className='fas fa-envelope list-item__icon standardIcon' /></a>}
                   {!contact.email && <i className='fas fa-envelope list-item__hidden' />}
                 </div>
               </li>
